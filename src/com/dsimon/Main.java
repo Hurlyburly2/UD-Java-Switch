c6a1433bb38f0efb22ce40ae67a4e4ba30ecb099
Hurlyburly2 <dougdougmann@gmail.com>
2019-06-20 20:10:59 -0400

    challenge done

--- src/com/dsimon/Main.java
+++ src/com/dsimon/Main.java
@@ -31,5 +31,40 @@ public class Main {
             default:
                 System.out.println("Value was not 1 or 2");
         }
+
+        char letter = 'A';
+        switch(letter) {
+            case 'A':
+                System.out.println("Found an A");
+                break;
+            case 'B':
+                System.out.println("Found a B");
+                break;
+            case 'C':
+                System.out.println("Found a C");
+                break;
+            case 'D':
+                System.out.println("Found a D");
+                break;
+            case 'E':
+                System.out.println("Found an E");
+                break;
+            default:
+                System.out.println("Did not find A, B, C, D, or E");
+                break;
+        }
+
+        String month = "JANUARy";
+        switch(month.toLowerCase()) {
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not Sure");
+                break;
+        }
     }
 }
